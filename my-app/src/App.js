b3d319378f09eb144f9ef4c6e894f33a1c7c2d32
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2022-11-04 19:34:34 +0530

    React-1

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import ParentComponent from './components/ParentComponent';
+// import Counter from './components/Counter';
+// import { Greet, ReactGreet } from './components/Greet';
+// import Greeting from './components/Greeting';
+// import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Greet />
+      <ReactGreet /> */}
+      {/* <Greeting name="JavaScript">Language of the Web</Greeting>
+      <Greeting name="Golang" />
+      <Greeting name="Swift">Language for iOS apps</Greeting>
+      <hr />
+      <Welcome name="Flutter">Language for cross-mobile apps</Welcome>
+      <Welcome name="Java" />
+      <Welcome name="React Native">Language for cross-mobile apps</Welcome> */}
+      {/* <Counter /> */}
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
