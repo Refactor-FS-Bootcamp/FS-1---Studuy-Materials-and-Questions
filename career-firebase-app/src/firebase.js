2e8469d49848ce39f1ba1b1ea2a391c0567856c0
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2022-11-17 17:04:10 +0530

    career-app

---
+++ career-firebase-app/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCMCzRekS2KaCINkq9-NoVYzra9z4Tb3LM",
+    authDomain: "tinder-firebase-hashout.firebaseapp.com",
+    projectId: "tinder-firebase-hashout",
+    storageBucket: "tinder-firebase-hashout.appspot.com",
+    messagingSenderId: "504248710713",
+    appId: "1:504248710713:web:a86184fd396c26f8c1bfe8"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth, db }
